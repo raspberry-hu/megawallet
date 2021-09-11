a16752197b89aafb57ffe18513b405e6505e3cf9
raspberry-hu <xuchong42@gmail.com>
2021-09-11 20:15:09 +0800

    Initial Commit

---
+++ megawallet/megawalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  megawalletApp.swift
+//  megawallet
+//
+//  Created by hu on 2021/9/11.
+//
+
+import SwiftUI
+
+@main
+struct megawalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
